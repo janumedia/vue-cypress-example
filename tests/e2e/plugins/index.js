3530eb0fde591e5d7f1ff11b7ecb1db18d5c4c03
janumedia <janumedia@gmail.com>
2020-06-05 09:46:28 +0800

    update webpackOptions

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -5,24 +5,16 @@
 // as explained in the cypress docs
 // https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
 
-/* eslint-disable import/no-extraneous-dependencies, global-require, arrow-body-style */
 const webpackPreprocessor = require("@cypress/webpack-preprocessor");
 
+// find webpack settings
 const fw = require("find-webpack");
 const webpackOptions = fw.getWebpackOptions();
 
 module.exports = (on, config) => {
-  //on('file:preprocessor', webpack({
-  // webpackOptions: require('@vue/cli-service/webpack.config'),
-  // watchOptions: {}
-  //}))
+  on("file:preprocessor", webpackPreprocessor({ webpackOptions }));
 
-  on(
-    "file:preprocessor",
-    webpackPreprocessor({
-      webpackOptions: require("@vue/cli-service/webpack.config")
-    })
-  );
+  require("@cypress/code-coverage/task")(on, config);
 
   return Object.assign({}, config, {
     fixturesFolder: "tests/e2e/fixtures",
